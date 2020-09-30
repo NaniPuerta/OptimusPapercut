dd77adc4f41151186de63349df76bd02d1e537b0
e.puerta <e.puerta@estudiantes.matcom.uh.cu>
2020-09-30 00:13:43 -0400

    First commit

---
+++ OptimizationCore/Core.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OptimizationCore
+{
+    public class CutPattern
+    {
+        public CutPattern()
+        {
+
+        }
+    }
+
+    public class Simplex
+    {
+        public Simplex()
+        {
+
+        }
+    }
+
+    public class Knapsack
+    {
+        public Knapsack()
+        {
+
+        }
+    }
+}
