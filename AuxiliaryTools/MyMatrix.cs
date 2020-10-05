d7b6de6a472fa135fe8063e99d160bb151dcabcd
e.puerta <e.puerta@estudiantes.matcom.uh.cu>
2020-10-05 16:10:55 -0400

    Finished implementing basic auxiliary matrix class

---
+++ AuxiliaryTools/MyMatrix.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+
+
+namespace AuxiliaryTools
+{
+    public class MyMatrix : IEnumerable
+    {        
+        private double[,] elements;
+        public int RowCount { get { return this.elements.GetLength(0); } }
+        public int ColCount { get { return this.elements.GetLength(1); } }
+
+        public int Count { get { return this.elements.Length; } }
+        public int Rank { get { return this.elements.Rank; } }
+
+        
+        public MyMatrix(double[,] elems)
+        {
+            this.elements = new double[elems.GetLength(0), elems.GetLength(1)];
+            Array.Copy(elems, elements, elems.Length);
+            
+        }
+
+        public MyMatrix(int length1, int length2)
+        {
+            this.elements = new double[length1, length2];
+        }
+
+        public MyMatrix(double[] elems)
+        {
+            elements = new double[1, elems.Length];
+            for (int i = 0; i < elems.Length; i++)
+            {
+                elements[1, i] = elems[i];
+            }
+        }
+
+        public MyMatrix(int[] elems)
+        {
+            elements = new double[1, elems.Length];
+            for (int i = 0; i < elems.Length; i++)
+            {
+                elements[1, i] = elems[i];
+            }
+        }
+
+        public double this[int i, int j]
+        {
+            get { return elements[i, j]; }
+            set { elements[i, j] = value; }
+        }
+
+        public static MyMatrix operator +(MyMatrix a) => a;
+        
+        public static MyMatrix operator -(MyMatrix a)
+        {
+            return Negate(a);
+        }
+
+        public static MyMatrix operator +(MyMatrix a, MyMatrix b)
+        {
+            return Sum(a, b);
+        }
+
+        public static MyMatrix operator -(MyMatrix a, MyMatrix b)
+        {
+            return Substract(a, b);
+        }
+
+        public static MyMatrix operator *(MyMatrix a, MyMatrix b)
+        {
+            return Multiply(a, b);
+        }
+
+        public static MyMatrix Negate(MyMatrix a)
+        {
+            int rows = a.RowCount;
+            int cols = a.ColCount;
+            MyMatrix result = new MyMatrix(rows, cols);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = -a[i, j];
+                }
+            }
+            return result;
+        }
+
+        public static MyMatrix Sum(MyMatrix a, MyMatrix b)
+        {
+            int rows = a.RowCount;
+            int cols = a.ColCount;
+            MyMatrix result = new MyMatrix(rows, cols);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return result;
+        }
+
+        public static MyMatrix Substract(MyMatrix a, MyMatrix b)
+        {
+            int rows = a.RowCount;
+            int cols = a.ColCount;
+            MyMatrix result = new MyMatrix(rows, cols);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = a[i, j] - b[i, j];
+                }
+            }
+            return result;
+        }
+
+        public static MyMatrix Multiply(MyMatrix a, MyMatrix b)
+        {
+            int arows = a.RowCount;
+            int cols = a.ColCount;
+            int bcols = b.ColCount;
+            MyMatrix result = new MyMatrix(arows, bcols);
+            for (int i = 0; i < arows; i++)
+            {
+                for (int j = 0; j < bcols; j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < cols; k++)
+                    {
+                        temp += a[i, k] * b[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }
+            return result;
+        }
+
+        public static void Invert(MyMatrix a)
+        {
+            int rows = a.RowCount;
+            int cols = a.ColCount;
+            int i = 0;
+            int j = 0;
+            while (i < rows && j < cols)
+            {
+                try
+                { 
+                    a[i, j] = 1 / a[i, j];
+                }
+                catch (DivideByZeroException)
+                {
+                    a[i, j] = 0;  
+                }
+                i++;
+                j++;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return elements.GetEnumerator();
+        }        
+    }
+}
--- OptimizationCore/Core.cs
+++ OptimizationCore/Core.cs
@@ -1,20 +1,131 @@
 ﻿using System;
+using System.Numerics;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 
 namespace OptimizationCore
 {
+    public class OptimizationProblem
+    {
+        public (int, int, int) stock { get; private set; }
+        public (int, int)[] pieces { get; private set; }
+        public int[] demands { get; private set; }
+        public int size { get; private set; }
+
+        public CutPattern[] solution { get; }
+
+        public OptimizationProblem(int stockLength, int stockWidth, int stockCost, int[] piecesLength, int[] piecesWidth, int[] piecesDemand)
+        {
+            this.size = piecesLength.Length;
+            this.stock = (stockWidth, stockLength, stockCost == 0 ? 1 : stockCost);
+            this.pieces = new (int, int)[this.size];
+            for (int i = 0; i < this.size; i++)
+            {
+                pieces[i] = (piecesWidth[i], piecesLength[i]);
+            }
+            this.demands = new int[this.size];
+            Array.Copy(piecesDemand, this.demands, this.size);
+        }
+
+    }
     public class CutPattern
     {
+        public double[] variant_vector { get; private set; }
+        public int cost { get; private set; }
         public CutPattern()
         {
+            this.variant_vector = new double[0];
+        }
 
+        public CutPattern(double[] variant, int cost)
+        {
+            this.variant_vector = new double[variant.Length];
+            Array.Copy(variant, this.variant_vector, variant.Length);
+            this.cost = cost;
         }
     }
 
     public class Simplex
     {
+        private OptimizationProblem problem;
+        private double[,] invB;
+        private double[] dt;
+        private int[] varIndex;
+        private double fovalue;
+        private int[] cb;
+        private double[] invBb;
+
         public Simplex()
         {
 
+        }
+
+        public Simplex(OptimizationProblem problem)
+        {
+            this.invB = new double[problem.size,problem.size];
+            this.problem = problem;
+            this.dt = new double[problem.size];
+            this.varIndex = new int[problem.size];
+            this.fovalue = 0;
+            this.cb = new int[problem.size];
+            this.invBb = new double[problem.size];
+        }
+
+        public CutPattern[] Solve( CutPattern[] initialsolution)
+        {
+            for (int i = 0; i < this.problem.size; i++)
+            {
+                this.cb[i] = initialsolution[i].cost;
+                this.varIndex[i] = i;
+                for (int j = 0; j < this.problem.size; j++)
+                {
+                    this.invB[i, j] = initialsolution[i].variant_vector[j];
+                }
+
+                this.invBb[i] = this.invB[i,i] * this.problem.demands[i];
+            }
+            this.fovalue = MultiplyColumns(this.cb, this.invBb);
+
+            while (true)
+            {
+                CutPattern entering = GenerateColumn();
+                double newcost = entering.cost - MultiplyColumns(this.dt, entering.variant_vector);
+                if (newcost >= 0)
+                {
+                    break;
+                }
+
+            }
+
+            return null;
+        }
+
+        private CutPattern[] GenerateInitialSolution()
+        {
+            throw new NotImplementedException();
+        }
+
+        private CutPattern GenerateColumn()
+        { throw new NotImplementedException(); }
+
+        private double MultiplyColumns(double[] column1, double[] column2)
+        {
+            double value = 0;
+            for (int i = 0; i < column1.Length; i++)
+            {
+                value += column1[i] * column2[i];
+            }
+            return value;
+        }
+
+        private double MultiplyColumns(int[] column1, double[] column2)
+        {
+            double value = 0;
+            for (int i = 0; i < column1.Length; i++)
+            {
+                value += column1[i] * column2[i];
+            }
+            return value;
         }
     }
 
